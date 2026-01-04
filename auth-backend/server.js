008e229e1cc37cf241fc86c9191bdd84e39f099a
murali-krishna-palla <pmuralikrishna520@gmail.com>
2026-01-04 10:39:25 +0530

    Update server.js

--- auth-backend/server.js
+++ auth-backend/server.js
@@ -9,18 +9,24 @@ connectDB();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://auth-ernbcp6mv-muralis-projects-53a36327.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-// Test route
+// Health check
 app.get("/", (req, res) => {
   res.send("Auth Backend Running");
 });
 
 // Auth routes
-console.log("AuthRoutes import:", require("./routes/authRoutes"));
 app.use("/api/auth", require("./routes/authRoutes"));
-
 
 const PORT = process.env.PORT || 5000;
 
